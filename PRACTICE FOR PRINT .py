ab2603adf7db1df37b230702ccd3da6c6a0f6842
Zaynab Afzal <zainabafzal8888@gmail.com>
2024-10-14 11:03:50 +0400

    Add files via upload

---
+++ PRACTICE FOR PRINT .py
@@ -0,0 +1,23 @@
+print("Hello, World1!") #Basic String Output
+print(5+3) #Simple Math Inside Print 
+print("Python", "is", "fun!") # Multiple Arguments in Print 
+print("She said,\"Hello!\"") #Print with Escape Characters
+print("First line\nSecond line") #Newline Character 
+print("Item 1\tltem2\tltem3") #Tab Character 
+print("The result is", 42) #Print Multiple Data Types Together 
+print(12345) #Print a Single Number 
+print(3.14159) #Print a Decimal Number
+print("Dollar sign:$") #Print Special Characters
+print("apple","banana","cherry",sep=",") #Using the Sep Parameter
+print("Hello,",end="") 
+print("World!") #Using the end Parameter
+print("Python!"*3) #Print a Repeated String
+print("\u2764") #Prints a heart symbol       
+print() #Print Empty Line
+print("Hello"+"World!") #concatenating Strings Inside Print
+print(True)
+print(False) #Print Boolean Values
+print(1.2e3) #Prints 1200.0
+print("""This is a Multiline
+print statemen.""") #Print Multiline Text Using Tripple Quotes
+print("This is a backlash:\\") #Print Using Escaped Backlash
